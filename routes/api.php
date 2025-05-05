12da5768e360930de3c41b7dfbc5c6a09cd4028b
Aslam Mahmud Siam <aslam.mahmud@brainstation-23.com>
2025-05-05 15:44:42 +0600

    feat(api): add categories resource route to API for category management

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\v1\AuthController;
 use App\Http\Controllers\Api\v1\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\CategoryController;
 
 Route::name('api.v1.')->prefix('v1')->group(function () {
     Route::post('login', [AuthController::class, 'login'])->name('login');
@@ -12,8 +13,10 @@ Route::name('api.v1.')->prefix('v1')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('books', BookController::class);
         Route::post('books/{book}/stock', [BookController::class, 'stock'])->name('books.stock');
+
         Route::post('/book-loan-request', [BookController::class, 'requestBookLoan']);
         Route::get('/book-loans', [BookController::class, 'getBookLoans']);
         Route::put('/book-loans/{id}/request-due-date', [BookController::class, 'requestUpdateDueDate']);
+        Route::resource('categories', CategoryController::class);
     });
 });
