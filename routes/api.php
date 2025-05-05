9cadf76e10d764d690f15515f8b1e207374c22d3
Aslam Mahmud Siam <aslam.mahmud@brainstation-23.com>
2025-05-05 12:00:12 +0600

    api(auth): login api setup

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::name('api.v1.')->prefix('v1')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
